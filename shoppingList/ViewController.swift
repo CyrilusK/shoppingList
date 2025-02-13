7af855d3a54c5ce78ddf8dbc78eb16f8e5a0803c
CyrilusK <kcyrg17@gmail.com>
2025-02-13 13:43:44 +0300

    initial project

---
+++ shoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shoppingList
+//
+//  Created by Cyril Kardash on 13.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
